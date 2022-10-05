f306641ca566f9a59461296c94e5dfd55f9c53da
Janio Florencio dos Santos <info.janio@gmail.com>
2022-10-05 13:11:53 -0300

    criacao de snippes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-import { Container } from './styles'
+import { Container, Title } from './styles'
 
 export function Home() {
-  return <Container></Container>
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  )
 }
